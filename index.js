c3708ed4d4b252560bec9151ccd5d228966a5789
adamjmcgrath <adamjmcgrath@gmail.com>
2016-12-06 22:36:05 +0000

    Tumblr

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import { curry, mergeAll } from 'ramda';
 
 export default curry((provider, platform, opts) =>
-  provider.authUrl(
-    platform.request, mergeAll([platform.opts, provider.opts, opts]),
-  )
-  .then(platform.dance)
-  .then(provider.accessToken)
-  .then(provider.identify(platform.request, opts)),
+  provider.authorize(platform, mergeAll([provider.opts, platform.opts, opts]))
+          .then(provider.identify(platform.request)),
 );
