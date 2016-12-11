7c9b02e8dccbb68a05f60ce63f48bcaf3257023e
adamjmcgrath <adamjmcgrath@gmail.com>
2016-12-11 13:05:13 +0000

    Publishing

---
+++ providers.js
@@ -0,0 +1,7 @@
+export { default } from './index';
+export { default as facebook } from './lib/providers/facebook';
+export { default as google } from './lib/providers/google';
+export { default as instagram } from './lib/providers/instagram';
+export { default as linkedin } from './lib/providers/linkedin';
+export { default as tumblr } from './lib/providers/tumblr';
+export { default as twitter } from './lib/providers/twitter';
